30dc29d3ce200f32eb10775919d3cb29f10a81e4
Tim Knowles <tim@Tims-MacBook-Air.local>
2016-04-26 20:47:23 +0100

    tunnel address change

--- main.rb
+++ main.rb
@@ -39,7 +39,7 @@ __END__
 
       $.ajax({
         type: 'post',
-        url: 'https://73fd7353.ngrok.io/start',
+        url: 'https://252a7d04.ngrok.io/start',
         dataType: 'json',
         complete: function(data) {
            $("#message").val(data.responseText);
@@ -51,7 +51,7 @@ __END__
 
       $.ajax({
         type: 'post',
-        url: 'https://73fd7353.ngrok.io/stop',
+        url: 'https://252a7d04.ngrok.io/stop',
         dataType: 'json',
         complete: function(data) {
            $("#message").val(data.responseText);
@@ -64,7 +64,7 @@ __END__
       $.ajax({
         type: 'post',
         contentType: 'application/json',
-        url: 'https://73fd7353.ngrok.io/temp',
+        url: 'https://252a7d04.ngrok.io/temp',
         data: JSON.stringify ({"temp":$('#temp').val()}),
         dataType: 'json',
         complete: function(data) {
